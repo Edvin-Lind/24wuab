5e432a0a23df7d769de9a57c96ac6fe8478260ac
Edvin-Lind <152278889+Edvin-Lind@users.noreply.github.com>
2024-03-24 20:53:56 +0100

    2024-03-24 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+document.querySelector(".fade-layer").addEventListener("click", showMenu);
 
+document.querySelector(".menu-button").addEventListener("click", showMenu);
+
+function showMenu() {
+  let menu = document.querySelector("nav.menu");
+  menu.classList.toggle("show");
+
+  let layer = document.querySelector(".fade-layer");
+  layer.classList.toggle("visible");
+}
+
+document.querySelectorAll(".content").forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("expanded");
+    // You can toggle additional information or styling changes here
+  });
+});
